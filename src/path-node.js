ae4d39810fa902e0eaa2cb3b4d8546651e8cf3fa
nayruthCalla <nayruth.calla@gmail.com>
2019-06-06 21:00:34 -0500

    closed #2

---
+++ src/path-node.js
@@ -0,0 +1,10 @@
+import module from 'path'
+import modulefs from 'fs'
+// import {fs} from 'fs'
+// const fs = require('fs')
+export const isAbsoluteRuta = module.isAbsolute;
+export const convAbso = module.resolve;
+export const extention = module.basename;
+export const extName = module.extname;
+export const isExit = modulefs.exists;
+export const isDir = modulefs.readdir;
