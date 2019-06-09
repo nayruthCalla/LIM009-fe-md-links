33aee414d7809514b5e9f227300a364c3a4c31a5
nayruthCalla <nayruth.calla@gmail.com>
2019-06-09 13:56:47 -0500

    path absolute closed #4

--- src/pathFiles.js
+++ src/pathFiles.js
@@ -1,19 +1,33 @@
 import module from 'path'
 import modulefs from 'fs'
-export const isAbsoluteRuta = module.isAbsolute;
 export const convAbso = module.resolve;
-// export const isExit = modulefs.exists;
-export const isDir = modulefs.readdir;
+export const extName = module.extname;
 export const isFile = modulefs.stat;
-
 export const pathFile = (path)=>{
-   return new Promise((resolve,reject)=>{
-       isFile(path,(err,stats)=>{
-           if(err){
-               reject(err)
-           }
-           resolve(stats.isFile())
-       })
-   })
+    const absoltPath = convAbso(path)
+    return new Promise((resolve,reject)=>{
+        isFile(absoltPath,(err,stats)=>{
+            if(err){
+                reject(`no es una ruta ${path}`)
+            }
+            else if(stats.isFile()){
+                resolve(absoltPath)
+            }
+            else {
+                resolve(false)
+            }
+        })
+    })
 }
-pathFile('/home/nayruth/Escritorio/node-eaxmplo/Node.js').then(result => console.log(result))
+export const markdownFile = (path)=>{
+    const arrayPath = []
+    return pathFile(path).then(result => {
+        if(extName(result)==='.md'){
+            // console.log('si es md') console.log(arrayPath)
+            arrayPath.push(result)
+            return arrayPath           
+        }
+    })  
+}
+markdownFile('../node-eaxmplo/README.md').then(result => console.log(result))
+
